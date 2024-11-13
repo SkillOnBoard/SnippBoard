19acc3432f4404663831bba1d5b9f230b4b9bfdd
Rubén Medina <ruben.medina@factorial.co>
2024-11-13 17:08:24 +0100

    adding active line onKeyUp

--- src/renderer/src/components/atoms/StyledTextArea/index.tsx
+++ src/renderer/src/components/atoms/StyledTextArea/index.tsx
@@ -16,7 +16,7 @@ const StyledTextArea = ({
   required = false,
   disabled = false,
   numOfLines = 4
-}: Props) => {
+}: Props): JSX.Element => {
   const lineCount = useMemo(() => value.split('\n').length, [value])
   const [activeLine, setActiveLine] = React.useState(1)
   const linesArr = useMemo(
@@ -25,22 +25,16 @@ const StyledTextArea = ({
   )
 
   const lineCounterRef = useRef<HTMLDivElement>(null)
-  const textareaRef = useRef<HTMLTextAreaElement>(null)
 
   const handleTextareaChange = (event: React.ChangeEvent<HTMLTextAreaElement>): void => {
-    // Consider keydown???
-    setActiveLine(event.target.value.substr(0, event.target.selectionStart).split('\n').length)
     onChange(event)
   }
 
-  const handleTextareaScroll = () => {
-    if (lineCounterRef.current && textareaRef.current) {
-      lineCounterRef.current.scrollTop = textareaRef.current.scrollTop
-    }
+  const handleKeyUp = (event): void => {
+    setActiveLine(event.target.value.substr(0, event.target.selectionStart).split('\n').length)
   }
 
-  // TODO: Manage overflow-y to define a max-height and add scroll
-  // Also, add highlight to the active line
+  // TODO: Add highlight to the active line
   // and colors to the programming language
   return (
     <div className="grid grid-cols-12 bg-gray-700 border border-gray-600 text-sm rounded-lg block w-full p-2.5 outline-none placeholder:text-gray-300 max-h-40 overflow-y-scroll no-scrollbar">
@@ -57,7 +51,8 @@ const StyledTextArea = ({
         placeholder={placeholder}
         value={value}
         onChange={handleTextareaChange}
-        // onScroll={handleTextareaScroll}
+        onKeyUp={handleKeyUp}
+        onClick={handleKeyUp}
         required={required}
         disabled={disabled}
       />
