813f17347853023c21a584466954f0ac2d5330a0
Rubén Medina Zamorano <ruben.medina@factorial.co>
2025-01-21 17:52:58 +0100

    fest: edit snippet (#60)
    Allow to edit shifts from the list.
    
    ---------
    
    Co-authored-by: Jorge Regidor <jorgeregidor@gmail.com>

--- src/renderer/src/pages/Create/index.tsx
+++ src/renderer/src/pages/Create/index.tsx
@@ -1,25 +1,15 @@
-import LabeledInput from '@renderer/components/molecules/LabeledInput'
-import TagPicker from '@renderer/components/molecules/TagPicker'
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import StyledLabeledTextArea from '@renderer/components/molecules/StyledLabeledTextArea'
 import { useTranslation } from 'react-i18next'
 import { useCreateSnippet } from '@renderer/hooks/useCreateSnippet'
 import Layout from '@renderer/components/Layout'
-import { useListTags } from '@renderer/hooks/useListTags'
 import { useNotifications } from '@renderer/contexts/NotificationsContext'
-
-type CreateForm = {
-  title: string
-  content: string
-  labels: Label[]
-}
+import SnippetForm, { CreateForm } from '@renderer/components/SnippetForm'
 
 function Create(): JSX.Element {
   const navigate = useNavigate()
   const [form, setForm] = useState<CreateForm>({ title: '', content: '', labels: [] })
   const { t } = useTranslation()
-  const { data: predefinedTags } = useListTags()
   const { addNotification } = useNotifications()
 
   useEffect(() => {
@@ -65,37 +55,7 @@ function Create(): JSX.Element {
         }
       ]}
     >
-      <div className="p-4">
-        <form onSubmit={submit}>
-          <div className="grid gap-4">
-            <div className="grid grid-cols-2 gap-3">
-              <LabeledInput
-                label={t('create.fields.name.label')}
-                placeholder={t('create.fields.name.label')}
-                value={form.title}
-                onChange={(e) => setForm({ ...form, title: e.target.value })}
-                autofocus
-              />
-
-              <TagPicker
-                label={t('create.fields.label.label')}
-                placeholder={t('create.fields.label.placeholder')}
-                values={form.labels}
-                onChange={(values) => setForm({ ...form, labels: values })}
-                predefinedTags={predefinedTags}
-              />
-            </div>
-
-            <StyledLabeledTextArea
-              label={t('create.fields.code.label')}
-              placeholder={t('create.fields.code.placeholder')}
-              value={form.content}
-              numOfLines={6}
-              onChange={(e) => setForm({ ...form, content: e.target.value })}
-            />
-          </div>
-        </form>
-      </div>
+      <SnippetForm form={form} setForm={setForm} onSubmit={submit} />
     </Layout>
   )
 }
