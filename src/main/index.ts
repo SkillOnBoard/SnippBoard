36de18784c8005fdd0948622375643cd770c7829
Jorge Regidor <jorgeregidor@gmail.com>
2025-03-10 18:49:33 +0100

    changed icon

--- src/main/index.ts
+++ src/main/index.ts
@@ -9,6 +9,8 @@ import { Snippet, Label } from './models'
 
 let tray: Tray | null = null
 
+app.dock.setIcon(icon)
+
 const createTrayMenu = (mainWindow: BrowserWindow): void => {
   tray = new Tray(trayIcon)
 
@@ -83,6 +85,7 @@ function createWindow(): void {
     }
   })
 
+
   mainWindow.on('ready-to-show', () => {
     mainWindow.show()
   })
@@ -148,7 +151,7 @@ app.whenReady().then(async () => {
   electronApp.setAppUserModelId('com.electron')
 
   app.dock.setIcon(icon)
-  app.dock.hide()
+  // app.dock.hide()
 
   await runMigrations()
 
